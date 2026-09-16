33c390c594b738683229ac2272a16aaad42d2d01
JuanJo Ciarlante <juanjosec@gmail.com>
2026-09-16 15:13:20 -0300

    feat(promql): add experimental zscore and robust_zscore functions
    Squashed from jjo/add-zscore-function.
    
    Adds zscore/zscore_over_time and robust_zscore/robust_zscore_over_time,
    normalizing samples against the mean and standard deviation, or against
    the median and median absolute deviation for the robust variants, which
    are far less sensitive to the outliers that make plain z-scores
    unreliable on spiky monitoring data.
    
    Gated behind --enable-feature=promql-experimental-functions.
    
    Signed-off-by: JuanJo Ciarlante <juanjosec@gmail.com>

--- promql/parser/functions.go
+++ promql/parser/functions.go
@@ -427,6 +427,18 @@ var Functions = map[string]*Function{
 		ArgTypes:   []ValueType{ValueTypeMatrix},
 		ReturnType: ValueTypeVector,
 	},
+	"robust_zscore": {
+		Name:         "robust_zscore",
+		ArgTypes:     []ValueType{ValueTypeVector},
+		ReturnType:   ValueTypeVector,
+		Experimental: true,
+	},
+	"robust_zscore_over_time": {
+		Name:         "robust_zscore_over_time",
+		ArgTypes:     []ValueType{ValueTypeMatrix},
+		ReturnType:   ValueTypeVector,
+		Experimental: true,
+	},
 	"round": {
 		Name:       "round",
 		ArgTypes:   []ValueType{ValueTypeVector, ValueTypeScalar},
@@ -558,6 +570,18 @@ var Functions = map[string]*Function{
 		ArgTypes:   []ValueType{ValueTypeVector},
 		Variadic:   1,
 		ReturnType: ValueTypeVector,
+	},
+	"zscore": {
+		Name:         "zscore",
+		ArgTypes:     []ValueType{ValueTypeVector},
+		ReturnType:   ValueTypeVector,
+		Experimental: true,
+	},
+	"zscore_over_time": {
+		Name:         "zscore_over_time",
+		ArgTypes:     []ValueType{ValueTypeMatrix},
+		ReturnType:   ValueTypeVector,
+		Experimental: true,
 	},
 }
 
